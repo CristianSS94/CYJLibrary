2fbe7a587e8b36e65eabc72a65b1eeb779cc8443
Cristian Sánchez Serrano <146743530+CristianSS94@users.noreply.github.com>
2024-05-26 01:08:34 +0200

    Actualizar RoutesBooks.jsx

--- client/books/src/routes/RoutesBooks.jsx
+++ client/books/src/routes/RoutesBooks.jsx
@@ -24,6 +24,24 @@ export const RoutesBooks = () => {
         <Route path={ruta.registro} element={<UserRegister />} />
         <Route path={ruta.perfil} element={<UserProfile />} />
         <Route path={ruta.editarPerfil} element={<EditProfile />} />
+        //TODO
+        // complater editar el perfil.
+        // Que el usuario se borre de la web ?
+        // subir libros ?
+        // modificar libros ?
+        // borrar libros?
+        // Marcar como intercambiado? requeriria modifica la DB y añadir algo tipo is_intercambiado y que sea true o false. esto afecataria al muestreo de libros ya que podria suponer poder aplicar un filtro de libros disponibles? tipo vinted que aunque vendas algo, se puede quedar en el perfil como vendido.
+        // ver sus libros subidos?
+        // ver libros de otros usuarios
+        // mensajear a otro usuario.
+        // borrar mensajes.
+        // servidor falta el controlador de libros
+        // servidor falta el controlador de mensaes
+        // el controlador de editar usuario requierenmoficiones
+        // crear consultad a la db con sequelize
+        
+        
+        
       </Routes>
     </BrowserRouter>
   );
